e1984ead96e766a981b3212c89a9b7ef68bf6411
Nathanael Noblet <nathanael@noblet.ca>
2013-01-29 22:54:13 -0700

    useable array choice system

--- Form/Types/ArrayChoice.php
+++ Form/Types/ArrayChoice.php
@@ -2,21 +2,18 @@
 
 namespace NS\UtilBundle\Form\Types;
 
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-abstract class ArrayChoice extends ChoiceType implements \Serializable, \Iterator
+abstract class ArrayChoice extends AbstractType
 {
     protected $values = array();
 
     protected $current; 
 
-    protected $position;
-    
     public function __construct($value = null)
     {
         $this->current = ($value == null || !isset($this->values[$value])) ? 0 : $value;
-        $this->position = $this->current;
     }
 
     public function __toString()
@@ -49,66 +46,13 @@ abstract class ArrayChoice extends ChoiceType implements \Serializable, \Iterato
     {
         parent::setDefaultOptions($resolver);
         $resolver->setDefaults(array(
-            'data_class' => __CLASS__,
-            'choices'    => $this->values,
+            'choices'     => $this->values,
+            'empty_value' => 'Please Select...',
         ));
     }
 
     public function getParent()
     {
         return 'choice';
-    }
-    
-    // Serializer Implementation
-    /**
-     * @see \Serializable::serialize()
-     */
-    public function serialize()
-    {
-        return serialize(array(
-            $this->current,
-            $this->values,
-            $this->position,
-        ));
-    }
-
-    /**
-     * @see \Serializable::unserialize()
-     */
-    public function unserialize($serialized)
-    {
-        list (
-            $this->current,
-            $this->values,
-            $this->position,
-        ) = unserialize($serialized);
-    }
-    
-    // Iterator Implementations
-    public function current()
-    {
-        return $this->values[$this->position];
-    }
-    
-    public function key()
-    {
-        return $this->position;
-    }
-
-    public function next()
-    {
-        return ++$this->position;
-    }
-    
-    public function valid()
-    {
-        return isset($this->values[$this->position]);
-    }
-    
-    public function rewind()
-    {
-        reset($this->values);
-
-        $this->position = key($this->values);
     }
 }
