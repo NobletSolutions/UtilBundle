787710ce4f842c4b1da5beb51aefc8cccab5a1ed
Nathanael d. Noblet <nathanael@noblet.ca>
2014-05-23 14:36:02 -0600

    translatable set choice

---
+++ Form/Types/TranslatableSetChoice.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace NS\UtilBundle\Form\Types;
+
+use \JMS\TranslationBundle\Model\Message;
+
+/**
+ * Description of TranslatableSetChoice
+ *
+ * @author gnat
+ */
+abstract class TranslatableSetChoice extends SetChoice
+{
+    static function getTranslationMessages()
+    {
+        $class = get_called_class();
+        $obj   = new $class();
+        $res   = array();
+
+        foreach($obj->getValues() as $val)
+        {
+            if(is_numeric($val))
+                continue;
+
+            $message = new Message($val);
+            $res[]   = $message;
+        }
+
+        return $res;
+    }
+}
