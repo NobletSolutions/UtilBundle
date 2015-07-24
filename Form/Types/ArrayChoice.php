89b6a515971681b529e9bd7cb26c77f98220571f
Nathanael d. Noblet <nathanael@noblet.ca>
2015-07-23 19:27:14 -0600

    added out of range value

--- Form/Types/ArrayChoice.php
+++ Form/Types/ArrayChoice.php
@@ -11,23 +11,27 @@ use \Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
 abstract class ArrayChoice extends AbstractType implements \Iterator
 {
+    const OUT_OF_RANGE = -9999;
     const NO_SELECTION = -1;
 
     protected $groupedValues = null;
     protected $values  = array(self::NO_SELECTION => 'N/A');
     protected $current = self::NO_SELECTION;
 
+    /**
+     * @param null $value
+     */
     public function __construct($value = null)
     {
         if (!is_null($value)) {
             if (is_numeric($value)) {
-                if (!isset($this->values[$value]) && $value != self::NO_SELECTION) {
+                if (!isset($this->values[$value]) && $value != self::NO_SELECTION && $value != self::OUT_OF_RANGE) {
                     throw new \UnexpectedValueException('Invalid numeric choice value: ' . $value . ' for ' . get_called_class());
                 }
 
                 $this->current = $value;
             }
-            else if (is_string($value)) {
+            elseif (is_string($value)) {
                 foreach ($this->values as $key => $v) {
                     if (strcasecmp($v, $value) == 0) {
                         $this->current = $key;
@@ -40,37 +44,61 @@ abstract class ArrayChoice extends AbstractType implements \Iterator
         }
     }
 
+    /**
+     * @return string
+     */
     public function __toString()
     {
         return (isset($this->values[$this->current]) ? $this->values[$this->current] : "");
     }
 
+    /**
+     * @param $value
+     */
     public function setValue($value)
     {
         $this->current = ($value == null || !isset($this->values[$value])) ? 0 : $value;
     }
 
+    /**
+     * @return array
+     */
     public function getValues()
     {
         return $this->values;
     }
 
+    /**
+     * @return int|string
+     */
     public function getValue()
     {
         return $this->current;
     }
 
+    /**
+     * @return array
+     */
     public function getLabels()
     {
         return $this->values;
     }
 
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $transformer = new ChoiceTransformer(get_called_class());
         $builder->addModelTransformer($transformer);
     }
 
+    /**
+     * @param FormView $view
+     * @param FormInterface $form
+     * @param array $options
+     */
     public function buildView(FormView $view, FormInterface $form, array $options)
     {
         if (isset($options['special_values'])) {
@@ -79,6 +107,9 @@ abstract class ArrayChoice extends AbstractType implements \Iterator
     }
 
     // Form AbstractType functions
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
     public function setDefaultOptions(OptionsResolverInterface $resolver)
     {
         $resolver->setDefaults(array(
@@ -91,16 +122,26 @@ abstract class ArrayChoice extends AbstractType implements \Iterator
         $resolver->addAllowedTypes(array('special_values' => 'array'));
     }
 
+    /**
+     * @return bool
+     */
     public function isValid()
     {
         return ($this->current != self::NO_SELECTION && isset($this->values[$this->current]));
     }
 
+    /**
+     * @return string
+     */
     public function getParent()
     {
         return 'choice';
     }
 
+    /**
+     * @param $var
+     * @return bool
+     */
     public function equal($var)
     {
         if (is_integer($var)) {
@@ -114,31 +155,49 @@ abstract class ArrayChoice extends AbstractType implements \Iterator
         return false;
     }
 
+    /**
+     * @return mixed
+     */
     public function current()
     {
         return current($this->values);
     }
 
+    /**
+     *
+     */
     public function next()
     {
         next($this->values);
     }
 
+    /**
+     * @return mixed
+     */
     public function key()
     {
         return key($this->values);
     }
 
+    /**
+     * @return bool
+     */
     public function valid()
     {
         return isset($this->values[$this->key()]);
     }
 
+    /**
+     *
+     */
     public function rewind()
     {
         reset($this->values);
     }
 
+    /**
+     *
+     */
     public function reverse()
     {
         $this->values = array_reverse($this->values);
--- Form/Types/TranslatableArrayChoice.php
+++ Form/Types/TranslatableArrayChoice.php
@@ -14,27 +14,44 @@ class ArraySource implements SourceInterface
 {
     private $name;
 
+    /**
+     * @param null $name
+     */
     public function __construct($name = null)
     {
         $this->name = $name;
     }
 
+    /**
+     * @return null
+     */
     public function getName()
     {
         return $this->name;
     }
 
+    /**
+     * @param $name
+     * @return $this
+     */
     public function setName($name)
     {
         $this->name = $name;
         return $this;
     }
 
+    /**
+     * @return null
+     */
     public function __toString()
     {
         return $this->name;
     }
 
+    /**
+     * @param SourceInterface $source
+     * @return bool
+     */
     public function equals(SourceInterface $source)
     {
         return ($source->__toString() == $this->name);
@@ -49,10 +66,10 @@ abstract class TranslatableArrayChoice extends ArrayChoice
         $obj   = new $class();
         $res   = array();
 
-        foreach($obj->getValues() as $val)
-        {
-            if(is_numeric($val))
+        foreach ($obj->getValues() as $val) {
+            if (is_numeric($val)) {
                 continue;
+            }
 
             $message = new Message($val);
 //            $message->addSource(new ArraySource("$class Choice"));
@@ -62,4 +79,3 @@ abstract class TranslatableArrayChoice extends ArrayChoice
         return $res;
     }
 }
-
