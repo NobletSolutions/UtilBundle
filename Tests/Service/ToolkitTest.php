d59889ea26cba4f84ec8c896d453d00058cd5edf
Nathanael d. Noblet <nathanael@noblet.ca>
2016-03-31 22:39:25 -0600

    Release of new version 2.1.0

---
+++ Tests/Service/ToolkitTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace NS\UtilBundle\Tests\Service;
+
+class ToolkitTest extends \PHP_Framework_TestCase
+{
+    public function testPregReplace()
+    {
+        $input = 'something &amp; and &#123; another";
+    }
+}
+
