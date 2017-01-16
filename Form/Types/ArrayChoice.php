f410b47a63aeb7959f14ab8927ba56b3fb0c250e
Nathanael d. Noblet <nathanael@noblet.ca>
2017-01-16 14:25:46 -0700

    fix missing use statement

--- Form/Types/ArrayChoice.php
+++ Form/Types/ArrayChoice.php
@@ -2,15 +2,16 @@
 
 namespace NS\UtilBundle\Form\Types;
 
-use \NS\UtilBundle\Form\Transformers\ChoiceTransformer;
-use \Symfony\Component\Form\AbstractType;
+use NS\UtilBundle\Form\Transformers\ChoiceTransformer;
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use \Symfony\Component\Form\FormBuilderInterface;
-use \Symfony\Component\Form\FormInterface;
-use \Symfony\Component\Form\FormView;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\Form\FormView;
+use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\OptionsResolver\Exception\InvalidOptionsException;
 use Symfony\Component\OptionsResolver\Options;
-use \Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 abstract class ArrayChoice extends AbstractType implements \Iterator
 {
